5736cd86e311852d58cca4b9ff60c4658a43ffe8
liyuchen <54489502+Liyuchen-1993@users.noreply.github.com>
2019-08-26 18:46:44 +0800

    Update RCode.R

--- RCode.R
+++ RCode.R
@@ -7,24 +7,22 @@ library(Rtsne)
 tsne <- Rtsne(matrix, dims=3, pca = F, initial_dims = 10, perplexity = 100, max_iter = 500)
 
 #the lasso and random forest algorithm for exLR-seq marker selection 
-n <- training.Norm.data <- training.data[,1: train.zero.sum]
-h <- training.PDAC.data <- training.data[,c(train.zero.sum+1): dim(training.data)[2]]
 library(glmnet)
 lasso.results <- c()
-for (j in 1:1000) {
-  n1 <- n[,sample(1:ncol(n),ceiling(dim(n)[2]*0.8),replace=F)]
-  h1 <- h[,sample(1:ncol(h),ceiling(dim(h)[2]*0.8),replace=F)]
-  s <- cbind(n1,h1)
-  ts <- t(s)
-  ts <- apply(ts,2,as.numeric)
+for (j in 1000:2000) {
+  n1 <- n[sample(1:nrow(n),ceiling(dim(n)[1]*0.8),replace=F),]
+  h1 <- h[sample(1:nrow(h),ceiling(dim(h)[1]*0.8),replace=F),]
+  s <- rbind.data.frame(n1,h1)
+  ts <- apply(s,2,as.numeric)
   y <- as.matrix(ts[,1])
-  x <- as.matrix(ts[,2:dim(data1)[1]])
+  x <- as.matrix(ts[,2:dim(ts)[1]])
   cv.fit <- cv.glmnet(x,y,family="binomial", type.measure = "auc", nfolds=5)
   co<-coef.cv.glmnet(cv.fit,s="lambda.1se")
   name <- rownames(co)[co[,1]!=0]
   lasso.results <- c(lasso.results, name)
 }
 freq.lasso.results <- table(lasso.results)
+
 library(varSelRF)
 facy <- factor(training.data$status)
 x <- training.data[,-1]
@@ -33,6 +31,7 @@ step=varSelRF(x, facy, c.sd = 1, mtryFactor = 1, ntree = 5000,
               whole.range = TRUE, recompute.var.imp = FALSE, verbose = FALSE,
               returnFirstForest = TRUE, fitted.rf = NULL, keep.forest = FALSE)
 select.history <- step$selec.history
+
 
 #The training diganostic model construction and SVM algorithm operation
 set.seed(1)
