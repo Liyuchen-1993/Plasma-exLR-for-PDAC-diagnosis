950a7c85e1b188fd925424b847557404791b5031
liyuchen <54489502+Liyuchen-1993@users.noreply.github.com>
2019-08-26 11:08:24 +0800

    Update RCode.R

--- RCode.R
+++ RCode.R
@@ -1,8 +1,10 @@
-
-
-#the tsne method for Dimension reduction and visualization 
+#Description
+#The core R codes for manuscript gutjnl-2019-318860
+#If you have any question of our source code, please contact me at yuchen_li@foxmail.com
+#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+#The tsne method for Dimension reduction and visualization 
 library(Rtsne)
-tsne <- Rtsne(matrix, dims=3, pca = F,initial_dims = 10,perplexity = 100,max_iter = 500)
+tsne <- Rtsne(matrix, dims=3, pca = F, initial_dims = 10, perplexity = 100, max_iter = 500)
 
 #the lasso and random forest algorithm for exLR-seq marker selection 
 n <- training.Norm.data <- training.data[,1: train.zero.sum]
@@ -17,7 +19,7 @@ for (j in 1:1000) {
   ts <- apply(ts,2,as.numeric)
   y <- as.matrix(ts[,1])
   x <- as.matrix(ts[,2:dim(data1)[1]])
-  cv.fit <- cv.glmnet(x,y,family="binomial",type.measure = "auc",nfolds=5)
+  cv.fit <- cv.glmnet(x,y,family="binomial", type.measure = "auc", nfolds=5)
   co<-coef.cv.glmnet(cv.fit,s="lambda.1se")
   name <- rownames(co)[co[,1]!=0]
   lasso.results <- c(lasso.results, name)
@@ -32,12 +34,13 @@ step=varSelRF(x, facy, c.sd = 1, mtryFactor = 1, ntree = 5000,
               returnFirstForest = TRUE, fitted.rf = NULL, keep.forest = FALSE)
 select.history <- step$selec.history
 
-#the training diganostic model construction and SVM algorithm operation
+#The training diganostic model construction and SVM algorithm operation
+set.seed(1)
 library(pROC)
 library(caret)
 library(e1071)
 fitControl <- trainControl(method = "repeatedcv",
-                           number = 10,
+                           number = 5,
                            repeats = 10,
                            ## Estimate class probabilities
                            classProbs = TRUE,
@@ -51,11 +54,13 @@ svmFit <- train(status ~ ., data = training.data,
                 preProc = c("center", "scale"),
                 tuneLength = 8,
                 metric = "ROC")
+
+#The establishment of d-signature from three cohort
 train.pred <- predict(svmFit,training.data)
 validation.pred <- predict(svmFit,validation.data)
 testing.pred <- predict(svmFit,testing.data)
 
-#the caculation of diagnostic efficacy
+#The caculation of diagnostic efficacy of d-signature
 train.con <- confusionMatrix(train.pred, training.data$status)
 validation.con <- confusionMatrix(validation.pred, validation.data$status)
 testing.con <- confusionMatrix(testing.pred, testing.data$status)
@@ -68,16 +73,9 @@ va.sp <- validation.con$byClass[2]
 te.acc <- testing.con$overall[1]
 te.se <- testing.con$byClass[1]
 te.sp <- testing.con$byClass[2]
-training.accuracy <- c(training.accuracy,tr.acc)
-training.sensitivity <- c(training.sensitivity,tr.se)
-training.specificity <- c(training.specificity,tr.sp)
-validation.accuracy <- c(validation.accuracy,va.acc)
-validation.sensitivity <- c(validation.sensitivity,va.se)
-validation.specificity <- c(validation.specificity,va.sp)
-testing.accuracy <- c(testing.accuracy,te.acc)
-testing.sensitivity <- c(testing.sensitivity,te.se)
-testing.specificity <- c(testing.specificity,te.sp)
-#
 
-
+#The Normfinder method for RT-qPCR
+source("r.NormOldStab5.txt")
+Result=Normfinder("Datafile.txt",ctVal=FALSE)
+selected.exLRs.normfinder.results <- Result$Ordered
 
